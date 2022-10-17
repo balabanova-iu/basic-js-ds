525fd52c58849aa59940b57797f2345f4008a34b
balabanova-iu <balabanova.iu@gmail.com>
2022-10-18 00:33:25 +0300

    feat: implement method add in binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,25 +1,47 @@
 const { NotImplementedError } = require("../extensions/index.js");
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require("../extensions/list-tree.js");
 
 /**
  * Implement simple binary search tree according to task description
  * using Node from extensions
  */
 class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
   root() {}
 
-  add(data) {}
+  add(data) {
+    let newNode = new Node(data);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
 
   has(/* data */) {
     throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   }
 
-  find(/* data */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
-  }
+  find(data) {}
 
   remove(/* data */) {
     throw new NotImplementedError("Not implemented");
